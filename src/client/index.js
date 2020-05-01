6ae768bfbbf78db00aa9b8aecaf249b15bdbe71b
Amir Abdulla <amir_abdulla@outlook.com>
2020-05-01 23:38:25 +0200

    First commit.

---
+++ src/client/index.js
@@ -0,0 +1,30 @@
+// Import statements
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+// Variable declarations
+let formBox = document.getElementById('formBox');
+let data = {};
+
+// Functions
+async function postData (url, data) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+}
+
+formBox.addEventListener('submit', () => {
+    data.text = document.getElementById('textBox').value;
+    console.log(data);
+    postData('/post', data);
+});
---
+++ src/server/index.js
@@ -0,0 +1,54 @@
+let data = {};
+
+// Dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Path
+var path = require('path')
+
+// Express
+const express = require('express')
+const app = express()
+app.use(express.static('dist'))
+app.use(express.json({limit: '3mb'}));
+
+/* Middleware*/
+const bodyParser = require('body-parser')
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Alyien API
+const aylien = require("aylien_textapi");
+const textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
+
+/*app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})*/
+
+app.listen(8080, function () {
+    console.log('Server listening on port 8080!')
+})
+
+app.post('/post', (req, res) => {
+    console.log('I got a request.')
+    data = req.body;
+    console.log(data);
+    textapi.classify({
+        url: data.text
+    }, function(error, response) {
+        if (error === null) {
+            console.log(response);
+            data = response;
+        } else {
+            console.log('This is not a valid text or article to evaluate. Try again.')
+        }
+    });
+    res.send(data);
+});
